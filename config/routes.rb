712477d15e572045062bc94097b6d0625f0913b2
미대생 <designer@midaesaeng-ui-MacBookPro.local>
2021-02-26 12:44:19 +0900

    aaa

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get 'home/third_page'
   get 'home/index'
   get '/' => 'home#index'
+  get '/sub_page' => 'home#sub_page'
+
+
 
   # get 'post/edit/:post_id' => 'post#edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
